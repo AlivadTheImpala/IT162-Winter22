70bdb2b9cfc4814525e959fbdc832c401adc181c
AlivadTheImpala <97662996+AlivadTheImpala@users.noreply.github.com>
2022-01-22 20:51:05 -0800

    Initial commit vie upload

---
+++ it162/includes/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Brandon Davilas Portal Page</title>
+    <meta name="robots" content="noindex,nofollow" />
+    <meta name="viewport" content="width=device-width" />
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="css/portal.css" />
+    <link rel="stylesheet" href="css/nav.css" />
+  </head>
+  <body>
+    <header>
+      <h1>Brandons Portal</h1>
+      <nav class="topnav" id="myTopnav">
+        <a href="index.php" class="active">Welcome</a>
+        <a href="big/index.php">Big</a>
+        <a href="aia.php">AIA</a>
+        <a href="flowchart.php">Flowchart</a>
+        <a href="fp/index.php">Final Project</a>
+        <a href="contactme.php">Contact Brandon</a>
+
+        <a href="http://w3schools.com" target="_blank">W3 Schools</a>
+
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()"
+          >&#9776;</a
+        >
+      </nav>
+    </header>
