3edc7476ea76d91de436d626ee0eb4b3397cc7de
Brandon <brandondavila315@outlook.com>
2022-01-22 20:25:12 -0800

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,43 @@
+<footer>
+        <p>
+          <small
+            >&copy; 2022 by <a href="contact.php">Brandon Davila</a>, All Rights
+            Reserved ~
+            <a id="html-checker" href="#">Check HTML</a>
+            ~
+            <a id="css-checker" href="#">Check CSS</a></small
+          >
+        </p>
+      </footer>
+    </div>
+
+    <!-- Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon -->
+
+    <script>
+      //https://tinyurl.com/dynamic-html-checker
+      document
+        .getElementById("html-checker")
+        .setAttribute(
+          "href",
+          "https://validator.w3.org/nu/?doc=" + location.href
+        );
+
+      document
+        .getElementById("css-checker")
+        .setAttribute(
+          "href",
+          "https://jigsaw.w3.org/css-validator/validator?uri=" + location.href
+        );
+
+      //manages mobile nav
+      function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+          x.className += " responsive";
+        } else {
+          x.className = "topnav";
+        }
+      }
+    </script>
+  </body>
+</html>
